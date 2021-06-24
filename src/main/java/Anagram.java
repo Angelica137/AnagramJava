1572281394151b579156cc538520fb37f9af20f8
Angelica <Angelica137@users.noreply.github.com>
2021-06-24 22:34:03 +0100

    list returns[lit] when [lit] passed, green

--- src/main/java/Anagram.java
+++ src/main/java/Anagram.java
@@ -1,3 +1,24 @@
-public class Anagram {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Anagram {
+	public List<String> isAnagram(String word, ArrayList<String> words) {
+		ArrayList<String> answer = new ArrayList<String>();
+		for (int i = 0; i < words.size(); i++) {
+			boolean isFound = words.get(i).indexOf(word) != 1 ? true : false;
+			if (isFound) {
+				System.out.println("true");
+				answer.add(words.get(i));
+			}
+		}
+		System.out.println(answer);
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Anagram a = new Anagram();
+		ArrayList<String> singleStr = new ArrayList<String>();
+		singleStr.add("lit");
+		a.isAnagram("list", singleStr);
+	}
 }
--- src/test/java/AnagramTets.java
+++ src/test/java/AnagramTets.java
@@ -1,3 +1,26 @@
+import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.Before;
+
 public class AnagramTets {
+	private Anagram a;
+	private ArrayList<String> singleStr;
+
+	@Before
+	public void setup() {
+		a = new Anagram();
+		singleStr = new ArrayList<String>();
+		singleStr.add("lit");
+	}
+
+	@Test
+	public void testAnagramReturnsAnagram() {
+		assertThat(a.isAnagram("list", singleStr)).containsExactlyInAnyOrder("lit");
+	}
 
 }
