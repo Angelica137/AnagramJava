f67e1801a77d19f4a4969fc5e8f4a8a30253f193
Angelica <Angelica137@users.noreply.github.com>
2021-06-26 22:54:43 +0100

    return anagram repeated letters

--- src/main/java/Anagram.java
+++ src/main/java/Anagram.java
@@ -5,11 +5,27 @@ public class Anagram {
 	public List<String> isAnagram(String word, ArrayList<String> words) {
 		ArrayList<String> answer = new ArrayList<String>();
 		for (int i = 0; i < words.size(); i++) {
+			System.out.println("first loop i " + i);
+			System.out.println("word is: " + words.get(i));
+
+			// check each character in it
+			// second loop
 			int count = 0;
 			for (int j = 0; j < words.get(i).length(); j++) {
+				System.out.println("second loop j " + j);
+				System.out.println(words.get(i).charAt(j));
+				System.out.println(word.charAt(j));
+				// against characters in the original word
+
 				for (int k = 0; k < word.length(); k++) {
+					System.out.println("third loop k " + k);
+					System.out.println(words.get(i).charAt(j));
+					System.out.println(word.charAt(k));
+
 					if (words.get(i).charAt(j) == word.charAt(k)) {
 						count++;
+						System.out.println("count " + count);
+						break;
 					}
 				}
 			}
@@ -19,5 +35,27 @@ public class Anagram {
 		}
 		System.out.println(answer);
 		return answer;
+	}
+
+	public static void main(String[] args) {
+		Anagram a = new Anagram();
+		ArrayList<String> singleStr = new ArrayList<String>();
+		singleStr.add("lit");
+		singleStr.add("otter");
+		a.isAnagram("lottery", singleStr);
+
+		// ArrayList<String> multStr = new ArrayList<String>();
+		// multStr.add("it");
+		// multStr.add("is");
+		// multStr.add("lit");
+		// multStr.add("nope");
+		// a.isAnagram("list", multStr);
+
+		// ArrayList<String> multStr2 = new ArrayList<String>();
+		// multStr2.add("it");
+		// multStr2.add("is");
+		// multStr2.add("lit");
+		// multStr2.add("otter");
+		// a.isAnagram("lottery", multStr2);
 	}
 }
--- src/test/java/AnagramTets.java
+++ src/test/java/AnagramTets.java
@@ -12,6 +12,7 @@ public class AnagramTets {
 	private Anagram a;
 	private ArrayList<String> singleStr;
 	private ArrayList<String> multStr;
+	private ArrayList<String> multStr2;
 
 	@Before
 	public void setup() {
@@ -23,6 +24,11 @@ public class AnagramTets {
 		multStr.add("is");
 		multStr.add("lit");
 		multStr.add("nope");
+		multStr2 = new ArrayList<String>();
+		multStr2.add("it");
+		multStr2.add("is");
+		multStr2.add("lit");
+		multStr2.add("otter");
 	}
 
 	@Test
@@ -33,6 +39,11 @@ public class AnagramTets {
 	@Test
 	public void testAnagramReturnsAngrams() {
 		assertThat(a.isAnagram("list", multStr)).containsOnly("it", "is", "lit");
+	}
+
+	@Test
+	public void testAnagramDiplicateLetters() {
+		assertThat(a.isAnagram("lottery", multStr2)).containsOnly("otter");
 	}
 
 }
