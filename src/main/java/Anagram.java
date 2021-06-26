3ac951db7d34de2a3c7ab1f79b7d31ecc4b4d232
Angelica <Angelica137@users.noreply.github.com>
2021-06-26 22:28:59 +0100

    delete obsolete

--- src/main/java/Anagram.java
+++ src/main/java/Anagram.java
@@ -4,28 +4,12 @@ import java.util.List;
 public class Anagram {
 	public List<String> isAnagram(String word, ArrayList<String> words) {
 		ArrayList<String> answer = new ArrayList<String>();
-		// for each word in the list
 		for (int i = 0; i < words.size(); i++) {
-			System.out.println("first loop i " + i);
-			System.out.println("word is: " + words.get(i));
-
-			// check each character in it
-			// second loop
 			int count = 0;
 			for (int j = 0; j < words.get(i).length(); j++) {
-				System.out.println("second loop j " + j);
-				System.out.println(words.get(i).charAt(j));
-				System.out.println(word.charAt(j));
-				// against characters in the original word
-
 				for (int k = 0; k < word.length(); k++) {
-					System.out.println("third loop k " + k);
-					System.out.println(words.get(i).charAt(j));
-					System.out.println(word.charAt(k));
-
 					if (words.get(i).charAt(j) == word.charAt(k)) {
 						count++;
-						System.out.println("count " + count);
 					}
 				}
 			}
@@ -35,20 +19,5 @@ public class Anagram {
 		}
 		System.out.println(answer);
 		return answer;
-	}
-
-	public static void main(String[] args) {
-		Anagram a = new Anagram();
-		ArrayList<String> singleStr = new ArrayList<String>();
-		singleStr.add("lit");
-		// singleStr.add("is");
-		a.isAnagram("list", singleStr);
-
-		// ArrayList<String> multStr = new ArrayList<String>();
-		// multStr.add("it");
-		// multStr.add("is");
-		// multStr.add("lit");
-		// multStr.add("nope");
-		// a.isAnagram("list", multStr);
 	}
 }
